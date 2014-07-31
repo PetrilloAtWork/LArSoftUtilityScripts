9f034cfeb90c6e2e90209390b757feba73c2d2f3
Gianluca Petrillo <petrillo@fnal.gov>
2014-07-31 10:44:40 -0500

    larstatus.sh: recursive git status command

---
+++ larstatus.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Executes the same GIT command on all git projects:
+# 
+# print the repository status
+# 
+# Run '--help' for help usage
+# (and keep in mind that the --git and --tag=PACKAGENAME options are always
+# applied)
+#
+
+SCRIPTDIR="$(dirname "$0")"
+
+"${SCRIPTDIR}/larcommands.sh" --git status "$@"
+
