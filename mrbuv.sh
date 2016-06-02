639012a01e3846d936a02025a2a3d4c04dfdd14d
Hamlet <hamletghost@gmail.com>
2016-06-02 11:34:19 -0500

    mrbuv.sh: better (?) handling of backup files

--- mrbuv.sh
+++ mrbuv.sh
@@ -13,18 +13,53 @@ fi
 
 cd "$MRB_SOURCE" || exit $?
 
+declare -r TempSuffix=".mrbuvsh"
+
+#
+# move existing backups out of the way
+#
+for ProductDepsBak in */ups/product_deps.bak ; do
+	RepoName="${ProductDepsBak%%/*}"
+	ProductDepsTempBackup="${ProductDepsBak}${TempSuffix}"
+	
+	echo "Moving away backup in ${RepoName}"
+	mv "$ProductDepsBak" "$ProductDepsTempBackup"
+done
+
+
+#
+# run `mrb uv`
+#
 [[ "$#" -gt 0 ]] && { mrb uv "$@" || exit $? ; }
 
+#
+# remove unnecessary backups
+#
 for ProductDepsBak in */ups/product_deps.bak ; do
+	RepoName="${ProductDepsBak%%/*}"
 	ProductDeps="${ProductDepsBak%.bak}"
+	ProductDepsTempBackup="${ProductDepsBak}${TempSuffix}"
 	if [[ ! -r "$ProductDeps" ]]; then
 		ERROR "Can't find the updated '${ProductDeps}' corresponding to the backup '${ProductDepsBak}'"
 		continue
 	fi
 	if cmp --quiet "$ProductDeps" "$ProductDepsBak" ; then
-		rm -vf "$ProductDepsBak"
+		# no difference, restore the backup(s)
+		mv "$ProductDepsBak" "$ProductDeps"
+		echo "No change in ${RepoName}."
+		[[ -r "$ProductDepsTempBackup" ]] && mv "$ProductDepsTempBackup" "$ProductDepsBak"
 	else
-		echo "Keeping '${ProductDepsBak}'."
+		# file is changed...
+		if [[ -r "$ProductDepsTempBackup" ]]; then
+			# ... and we have two backups.
+			# What do we do? the previous backup might be too outdated;
+			# or it might be the real one.
+			# We decide we keep the old one
+			mv "$ProductDepsTempBackup" "$ProductDepsBak"
+			echo "Changed ${RepoName} (keeping the existing backup)."
+		else
+			echo "Changed ${RepoName}."
+		fi
 	fi
 done
 
