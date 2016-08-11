611f6f06f72b09e56a9a81327d2791c0bb509687
Gianluca Petrillo <petrillo@fnal.gov>
2016-08-11 16:43:14 -0500

    largitlog.sh: prints one line of git log

---
+++ largitlog.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#
+# Executes the same GIT command on all git projects:
+# 
+# print the message of the last entry of the log
+# 
+# Run '--help' for help usage
+# (and keep in mind that the --git and --tag=PACKAGENAME options are always
+# applied)
+#
+
+SCRIPTDIR="$(dirname "$0")"
+
+function help() {
+  cat <<EOH
+
+Print the message of the last entry of the GIT log.
+
+Usage:  ${SCRIPTNAME}  [options]
+
+EOH
+  help_baseoptions
+  
+} # help()
+
+#
+# print the last entry of the log;
+# print it on a single line;
+# prepend the repository name first, aligned in 20 characters, before that line.
+#
+source "${SCRIPTDIR}/larcommands.sh" --compact='prepend-20' "$@" --git -- log -n 1 --oneline
