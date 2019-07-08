dacf3ddc0caea9236f96384b0d0ab0e0eb455204
Gianluca Petrillo <petrillo@slac.stanford.edu>
2019-07-08 11:15:05 -0500

    `create_area.sh`: updated default qualifiers

--- create_area.sh
+++ create_area.sh
@@ -5,7 +5,7 @@
 #
 
 declare local_create_area_DefaultVersion="${LARCORE_VERSION:-"develop"}"
-declare local_create_area_DefaultQual="${MRB_QUAL:-"e9:debug"}"
+declare local_create_area_DefaultQual="${MRB_QUAL:-"e17:debug"}"
 
 function IsInList() {
 	# IsInList Key ListItem [...]
